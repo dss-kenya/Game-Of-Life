196a2f1001a5a4ea378e7be431768a433295287b
Dhara <hearbeat.horror@gmail.com>
2016-06-09 18:21:27 +0530

    - adjusted the gridview

--- app/src/main/java/com/dhara/gameoflife/MainActivity.java
+++ app/src/main/java/com/dhara/gameoflife/MainActivity.java
@@ -4,10 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean state[][] = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        state = new boolean[][]
+                {
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                        {false,false,false,false,false,false,false,false,false,false},
+                };
     }
 }
---
+++ app/src/main/java/com/dhara/gameoflife/adapter/CellAdapter.java
@@ -0,0 +1,67 @@
+package com.dhara.gameoflife.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
+public class CellAdapter extends BaseAdapter {
+    private final Boolean mCellStates[][];
+    private final Context mContext;
+    private int totalCount;
+    private int rowPosition;
+    private int columnPosition;
+
+    public CellAdapter(Context context, Boolean states[][]) {
+        mCellStates = states;
+        mContext = context;
+        rowPosition = 0;
+        columnPosition = 0;
+        totalCount = getColumnCount() * getRowCount();
+    }
+
+    @Override
+    public int getCount() {
+        return totalCount;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView;
+        if (convertView == null) {
+            textView = new TextView(mContext);
+        }else {
+            textView = (TextView) convertView;
+        }
+        columnPosition = position % mCellStates[0].length;
+        rowPosition = (position - columnPosition)/mCellStates[0].length;
+        textView.setTextColor(mCellStates[rowPosition][columnPosition] ?
+                Color.parseColor("#000000") : Color.parseColor("#00000000"));
+        return textView;
+    }
+
+    public int getRowCount() {
+        return mCellStates.length;
+    }
+
+    public int getColumnCount(){
+        return mCellStates[0].length;
+    }
+
+    public Object getItem(int rowNum, int columnNum) {
+        return mCellStates[rowNum][columnNum];
+    }
+}
